988cf549bb4e4844037ce79c2f5937c4450a658d
Francis Papineau <francispapineau@gmail.com>
2015-03-14 14:17:49 -0400

    Started

--- server/server/apps/controller/models.py
+++ server/server/apps/controller/models.py
@@ -1,1 +1,30 @@
-__author__ = 'fpapinea'
+"""
+
+    models.py
+    ==========
+
+    This is the database models for the controller application
+
+    :copyright: (c) 2015 by GammaRay.
+    :license: BSD, see LICENSE for more details.
+
+    Author:         GammaRay
+    Version:        1.0
+    Date:           3/11/2015
+
+"""
+
+"""
+=============================================
+Imports
+=============================================
+"""
+
+import datetime
+from mongokit import Connection, Document, Database
+
+"""
+=============================================
+Source
+=============================================
+"""
